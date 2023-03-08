c07cdcc133ac531c76ccebb2f8dc1c15e608affc
Lincoln Lima <abrahamlima589@gmail.com>
2023-03-08 15:05:33 -0400

    correção do progama

--- MeuPrimeiroProgama.java
+++ MeuPrimeiroProgama.java
@@ -1,24 +1,7 @@
-    public class MeuPrimeiroProgama {
-        public static void main(String[]args){
-            System.out.println("08/03/2023");
-            System.out.println("Bem vindo!!!");
-            System.out.println("level 1");
-           System.out.println("....................");
-           System.out.println("....................");
-           System.out.println("....................");
-           System.out.println("....................");
-           System.out.println("....................");
-           System.out.println(";;;;;;;;;;;;;;;;;;;;");
-
-           sum("boa,noite");
-     }
-
-        static void sum(char);{
-            System.out.println(char);
-        }
-
+public class MeuPrimeiroProgama {
+    public static void main(String[] args){
+        System.out.println("08/03/2023");
+        System.out.println("Bem vindos!!!");
+        System.out.println("Teste do github.");
+    }
 }
-
-
-
-
