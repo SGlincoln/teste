2f94a356ec943c19c53a40ecadab1dbbe92a2c23
Lincoln Lima <abrahamlima589@gmail.com>
2023-03-08 14:31:02 -0400

    primeiro teste

---
+++ MeuPrimeiroProgama.java
@@ -0,0 +1,24 @@
+    public class MeuPrimeiroProgama {
+        public static void main(String[]args){
+            System.out.println("08/03/2023");
+            System.out.println("Bem vindo!!!");
+            System.out.println("level 1");
+           System.out.println("....................");
+           System.out.println("....................");
+           System.out.println("....................");
+           System.out.println("....................");
+           System.out.println("....................");
+           System.out.println(";;;;;;;;;;;;;;;;;;;;");
+
+           sum("boa,noite");
+     }
+
+        static void sum(char);{
+            System.out.println(char);
+        }
+
+}
+
+
+
+
